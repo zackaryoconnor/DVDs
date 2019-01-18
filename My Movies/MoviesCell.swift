7f4f1d823796b47f91324b5df27830e36979e0a1
zackaryoconnor <zackary25@gmail.com>
2019-01-18 13:30:00 -0800

    Setup basic collectionView for the main controller.

---
+++ My Movies/MoviesCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesCell.swift
+//  My Movies
+//
+//  Created by Zackary O'Connor on 1/18/19.
+//  Copyright © 2019 Zackary O'Connor. All rights reserved.
+//
+
+import UIKit
+
+class MoviesCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupCell()
+    }
+    
+    func setupCell() {
+        backgroundColor = .red
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ My Movies/MoviesController.swift
@@ -0,0 +1,57 @@
+//
+//  MoviesController.swift
+//  My Movies
+//
+//  Created by Zackary O'Connor on 1/18/19.
+//  Copyright © 2019 Zackary O'Connor. All rights reserved.
+//
+
+import UIKit
+
+class MoviesController: UICollectionViewController {
+
+    private let cellId = "Cell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .white
+        collectionView.register(MoviesCell.self, forCellWithReuseIdentifier: cellId)
+    }
+
+
+}
+
+extension MoviesController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 2
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as? MoviesCell {
+            return cell
+        }
+        
+        return UICollectionViewCell()
+    }
+}
+
+
+extension MoviesController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width / 2 - 24, height: 300)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+}

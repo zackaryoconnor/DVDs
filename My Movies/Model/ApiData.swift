dcd13eb265a6f989817308040c977fb3c6263f4c
zackaryoconnor <zackary25@gmail.com>
2019-01-18 14:44:55 -0800

    Setup basic data model.

---
+++ My Movies/Model/ApiData.swift
@@ -0,0 +1,27 @@
+//
+//  ApiData.swift
+//  My Movies
+//
+//  Created by Zackary O'Connor on 1/18/19.
+//  Copyright © 2019 Zackary O'Connor. All rights reserved.
+//
+
+import UIKit
+
+struct WebsiteResultData: Decodable {
+    let results: [Results]
+}
+
+struct Results: Decodable {
+    let id: Int
+    let posterPath: String
+    let title: String
+    let releaseDate: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case id, title
+    }
+}
+
